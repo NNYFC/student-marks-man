6fcb1cf87d361470c1eaf46262e03c36426a5792
NNYFC <nfrnanckcesar@gmail.com>
2024-07-05 14:14:34 +0100

    basic endpoints

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
 app.get('/', (req, res)=>{
     res.send('Hello world!');
+});
+
+app.get('/student/:name', (req, res)=>{
+  let name = req.params.name;
+  res.send(`Hello ${name}!`);
+});
+
+app.post('/student', (req, res)=>{
+  let {name, email} = req.body
+  res.send({ 'name': name, 'email': email });
 });
 
 app.listen(port, ()=>{
