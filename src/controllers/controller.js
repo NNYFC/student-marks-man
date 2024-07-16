44ca9d050cafc3578b57d343aae82a2c657ab401
NNYFC <nfrnanckcesar@gmail.com>
2024-07-16 17:29:50 +0100

    input body validation

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,4 +1,5 @@
 const db = require('../db_config');
+const { validationResult } = require('express-validator');
 
 // Getting list of students
 exports.getAllStudents = async(req, res) =>{
@@ -8,7 +9,7 @@ exports.getAllStudents = async(req, res) =>{
         
         res.status(200).send({success:true, count: users.length, data: users});
     }catch(err){
-        res.status(500).send("Error fetching users: "+err.stack);
+        res.status(500).send({success:false, message:"Error fetching users: "+err.stack});
     }
     
 };
@@ -16,13 +17,18 @@ exports.getAllStudents = async(req, res) =>{
 // adding a student
 exports.saveStudent = async(req, res) =>{
     try{
-        let { name,email } = req.body;
-        const result = await db.query('INSERT INTO students(name,email) VALUES(?,?)', [name,email]);
-        const users = result;
-        
-        res.status(201).send({success:true, data: users});
+        const errors = validationResult(req).errors;
+        if(errors.length>0){
+            res.status(400).send({success:false, data:[], message: errors[0].msg});
+        }else{
+            let { name,email } = req.body;
+            const result = await db.query('INSERT INTO students(name,email) VALUES(?,?)', [name,email]);
+            const users = result;
+            
+            res.status(201).send({success:true, data: users, message: "Successfully saved"});
+        }
     }catch(err){
-        res.status(500).send("Error adding user: "+err.stack);
+        res.status(500).send({success:false, data:[], message:"Error adding user: "+err.stack});
     }
     
 };
@@ -30,14 +36,19 @@ exports.saveStudent = async(req, res) =>{
 // updating a student info
 exports.updateStudent = async(req, res) =>{
     try{
-        let { name,email } = req.body;
-        let id = req.query.id;
-        const result = await db.query('UPDATE students SET name = ?, email =? WHERE id = ?', [name,email,id]);
-        const users = result;
-        
-        res.status(201).send({success:true, data: users});
+        const errors = validationResult(req).errors;
+        if(errors.length>0){
+            res.status(400).send({success:false, data:[], message: errors[0].msg});
+        }else{
+            let { name,email } = req.body;
+            let id = req.query.id;
+            const result = await db.query('UPDATE students SET name = ?, email =? WHERE id = ?', [name,email,id]);
+            const users = result;
+            
+            res.status(201).send({success:true, data: users, message: "updated successfully"});
+        }
     }catch(err){
-        res.status(500).send("Error updating user: "+err.stack);
+        res.status(500).send({success:false, data:[], message:"Error updating user: "+err.stack});
     }
     
 };
@@ -51,7 +62,7 @@ exports.deleteStudent = async(req, res) =>{
         
         res.status(201).send({success:true, message: "Student deleted successfully"});
     }catch(err){
-        res.status(500).send("Error fetching users: "+err.stack);
+        res.status(500).send({success:false, message:"Error fetching users: "+err.stack});
     }
     
 };
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/controller')
+const { body } = require('express-validator');
 
 router.use((req, res, next) => {
     console.log('Time:', Date.now())
@@ -16,7 +17,7 @@ router.get('/student/:name', (req, res)=>{
   
 router.post('/student', controller.saveStudent);
 
-router.put('/student', controller.updateStudent);
+router.put('/student', [body('name').isLength({min:3}),body('email').isLength({min:7})], controller.updateStudent);
 
 router.delete('/student/:id', controller.deleteStudent);
 
